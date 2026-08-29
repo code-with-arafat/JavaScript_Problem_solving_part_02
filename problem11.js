27fabb7a7dc9ee4c4babae44211bfe1804db6f27
code-with-arafat <ferdousinfo00@gmail.com>
2026-08-29 22:46:18 +0600

    Multiply Each Element by 2

---
+++ problem11.js
@@ -0,0 +1,9 @@
+//Multiply Each Element by 2
+const numbers = [5, 10, 15, 20, 25];
+const doubledNumbers = [];
+
+for (const number of numbers) {
+    doubledNumbers.push(number * 2);
+}
+
+console.log(doubledNumbers);
