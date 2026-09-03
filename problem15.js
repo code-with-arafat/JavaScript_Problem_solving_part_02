41c23b5d82add08b6d113228bd7e5ccf6e175805
code-with-arafat <ferdousinfo00@gmail.com>
2026-09-03 22:26:16 +0600

    feat: solve length of longest substring using sliding window approach

---
+++ problem15.js
@@ -0,0 +1,23 @@
+function lengthOfLongestSubstring(s) {
+    let charSet = new Set();
+    let left = 0;
+    let maxLength = 0;
+
+    for (let right = 0; right < s.length; right++) {
+        while (charSet.has(s[right])) {
+            charSet.delete(s[left]);
+            left++;
+        }
+
+        
+        charSet.add(s[right]);
+        maxLength = Math.max(maxLength, right - left + 1);
+    }
+
+    return maxLength;
+}
+
+
+console.log(lengthOfLongestSubstring("abcabcbb"));
+console.log(lengthOfLongestSubstring("bbbbb")); 
+console.log(lengthOfLongestSubstring("pwwkew"));
